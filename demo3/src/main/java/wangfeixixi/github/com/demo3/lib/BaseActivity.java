619de1422fa28a7aeaa5dc935b22a39a737a8032
xuanyuanxixi <1020301980@qq.com>
2018-04-09 13:16:11 +0800

    init

---
+++ demo3/src/main/java/wangfeixixi/github/com/demo3/lib/BaseActivity.java
@@ -0,0 +1,41 @@
+package wangfeixixi.github.com.demo3.lib;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(initContentView());
+    }
+
+    public abstract int initContentView();
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        EventBus.getDefault().unregister(this);
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onMessageEvent(Object event) {/* Do something */}
+
+    @Subscribe(threadMode = ThreadMode.BACKGROUND)
+    public void onMessageEventa(Object event) {/* Do something */}
+
+}
---
+++ demo3/src/main/java/wangfeixixi/github/com/demo3/lib/BaseFragment.java
@@ -0,0 +1,8 @@
+package wangfeixixi.github.com.demo3.lib;
+
+/**
+ * Created by xuany on 2018/4/4.
+ */
+
+public class BaseFragment {
+}
